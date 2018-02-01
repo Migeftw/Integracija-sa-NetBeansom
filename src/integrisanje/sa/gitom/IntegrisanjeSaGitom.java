814624c3784e29cccafd700b441a030a92548321
Mige <Mige@DESKTOP-CLOPUBN>
2018-02-01 20:11:53 +0100

    Initial upload

---
+++ src/integrisanje/sa/gitom/IntegrisanjeSaGitom.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package integrisanje.sa.gitom;
+
+/**
+ *
+ * @author Mige
+ */
+public class IntegrisanjeSaGitom {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Woohoo - i have created a repository");
+    }
+    
+}
